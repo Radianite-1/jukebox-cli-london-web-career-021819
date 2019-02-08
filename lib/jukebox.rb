c9c07789764765165f0020cd4a4136b3be976df3
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-02-08 19:42:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -38,5 +38,23 @@ end
 
 def play(my_songs)
   
+  puts "Please enter a song name or number:"
+    
+  user_request = gets.chomp
   
+  songs.each_with_index do |song, i|
+    
+    if user_request.to_i == i
+      
+    puts "playing #{songs[i-1]}"
+    
+  elsif user_request == song 
+  
+  puts "playing #{song[i]}"
+  
+else puts "Invalid input, please try again"
+  
+end 
+
+end 
 
