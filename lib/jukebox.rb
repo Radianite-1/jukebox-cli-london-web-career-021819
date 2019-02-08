1f4466e5eb927b1dd1b4f4bcf78702dfa5953e1a
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-02-08 19:39:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,11 +12,15 @@ songs = [
 
 
 def help 
-  puts 
+
+help =  
 "I accept the following commands:
 - help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
 - exit : exits this program"
+
+puts help
+
 end 
 
